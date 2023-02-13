a9026600175af938d11d0e75754279b921aea5a5
amiralc <amira.abdrabou@esprit.tn>
2023-02-13 08:33:35 +0100

    integrate templates backoffice and frontoffice

---
+++ src/Controller/TemplateController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TemplateController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_template_admin')]
+    public function dashboard_index(): Response
+    {
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'TemplateController',
+        ]);
+    }
+
+    #[Route('/home', name: 'app_template')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'TemplateController',
+        ]);
+    }
+}
